ef6c86b46a5c3c44acc656e363043a9107edd5fa
nlsmllr <info@nils-mueller.com>
2026-06-14 18:37:44 +0200

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,9 @@ const geistMono = Geist_Mono({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
